157c5a9bd440058bba94bf1396dda80b72aee560
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-11-23 01:59:19 +0100

    chore: rename crates dir

--- crates/ecc/src/ops.rs
+++
@@ -1,5 +0,0 @@
-pub const trait Pow<Rhs = u128> {
-    type Output;
-    #[must_use = "this returns the result of the operation, without modifying the original"]
-    fn pow(self, exp: Rhs) -> Self::Output;
-}
